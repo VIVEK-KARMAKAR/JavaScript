c98e2e73117a6108a3429c54829e2ffa53a6f436
Vivek_Karmakar <kvrmv28@gmail.com>
2024-03-21 16:07:55 +0530

    data-types added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const userID = 2203253
+let name = "vivek"
+var age = 21
+
+console.table([userID, name , age]);
+
+/*
+prefer not to use var 
+because of its issue generation in block 
+and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,44 @@
+/*
+*************DATA-TYPES*************
+
+-------------primitive data types-------------
+There are 7 primitive data types :--
+
+    ~ String         "stringInstance"
+    ~ Number       32
+    ~ BigInt         8987937483434n
+    ~ Boolean       True
+    ~ Null          null
+    ~ Undefined 
+    ~ Symbol
+
+-------------non-primitive data types-------------
+
+There is only one non-primitive or complex data-type in JavaScript 
+    ~Object [user-defined data_type]
+*/
+
+let student = {                                      // object 
+
+    fullName: "Vivek Karmakar",                       // string
+    age: 21,                                        // number
+    contactNumber: 8797361285,                     // BigInt   (The bigint type represents the whole numbers that are larger than 2^53 – 1.)
+    referralName: null,                              // null
+    referralD: undefined,                            // undefined
+    admissionStatus: true,                          // boolean
+    admissionNumber: Symbol("NSU2203253"),        // symbol
+
+}
+// console.log(student);
+console.table([student]);                             // priniting the data in a tabular form
+// console.log(typeof(student));
+
+// to acces the data in a object we have to use instances using the "." (dot operator)
+
+// console.log(student.referralD);
+console.log(`The data type of referalID is ${typeof(student.referralD)}`);
+
+// console.log(student.admissionNumber);
+console.log(`The data type of Admission number is ${typeof(student.admissionNumber)}`);
+
+// console.table([student.fullName, student.age , student.contactName]);
